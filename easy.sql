b63ab0e846462b3b0c1b70ae6a21f4ed8d723495
arfmtr <arifamutiaraa@gmail.com>
2022-05-13 12:12:53 +0700

    update

--- easy.sql
+++ easy.sql
@@ -110,3 +110,72 @@ WHERE countrycode = 'JPN';
 SELECT MAX(population) - MIN(population)
 FROM city;
 
+-- 24. Samantha was tasked with calculating the average monthly salaries for all employees in the EMPLOYEES table, but did not realize her keyboard's 0 key was broken until after completing the calculation. She wants your help finding the difference between her miscalculation (using salaries with any zeros removed), and the actual average salary.
+--Write a query calculating the amount of error (i.e.: actual-miscalculated  average monthly salaries), and round it up to the next integer.
+SELECT CEIL(AVG(Salary)-AVG(REPLACE(Salary,'0',''))) 
+FROM EMPLOYEES;
+
+-- 25. We define an employee's total earnings to be their monthly salary x months worked, and the maximum total earnings to be the maximum total earnings for any employee in the Employee table. Write a query to find the maximum total earnings for all employees as well as the total number of employees who have maximum total earnings. Then print these values as 2 space-separated integers.
+SELECT salary * months AS ttlearnings, COUNT(*)
+FROM employee
+GROUP BY ttlearnings
+ORDER BY ttlearnings DESC --BEACUSE MAX 
+LIMIT 1;
+
+-- 26. Query the following two values from the STATION table:
+--The sum of all values in LAT_N rounded to a scale of 2 decimal places.
+--The sum of all values in LONG_W rounded to a scale of 2 decimal places.
+SELECT ROUND(SUM(LAT_N),2) AS lat, ROUND(SUM(LONG_W),2) AS lon
+FROM station;
+
+-- 27. Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than 38.7880 and less than 137.2345. Truncate your answer to 4 decimal places.
+SELECT ROUND(SUM(LAT_N),4)
+FROM station
+WHERE(lat_n) > 38.7880 AND (lat_n) < 137.2345;
+
+-- 28. Query the greatest value of the Northern Latitudes (LAT_N) from STATION that is less than 137.2345. Truncate your answer to 4 decimal places.
+SELECT ROUND(MAX(LAT_N),4)
+FROM station
+WHERE lat_n < 137.2345;
+
+-- 29. Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) in STATION that is less than 137.2345. Round your answer to 4 decimal places.
+SELECT ROUND(MAX(LONG_W),4)
+FROM station
+WHERE lat_n IN (SELECT MAX(LAT_N) FROM station WHERE lat_n < 137.2345);
+
+-- 30. Query the smallest Northern Latitude (LAT_N) from STATION that is greater than 38.7780. Round your answer to 4 decimal places.
+SELECT ROUND(MIN(LAT_N),4)
+FROM station
+WHERE lat_n > 38.7780;
+
+-- 31. Query the Western Longitude (LONG_W)where the smallest Northern Latitude (LAT_N) in STATION is greater than 38.7780. Round your answer to 4 decimal places.
+SELECT ROUND(MAX(LONG_W),4)
+FROM station
+WHERE lat_n IN (SELECT MIN(LAT_N) FROM station WHERE lat_n > 38.7780);
+
+-- 32. Query the Manhattan Distance between points P1 and P2 and round it to a scale of 4 decimal places.
+SELECT ROUND(MAX(LAT_N)-MIN(LAT_N) + MAX(LONG_W)-MIN(LONG_W),4)
+FROM station;
+
+-- 33. Query the Euclidean Distance between points P1 and P2 and format your answer to display 4 decimal digits.
+SELECT ROUND(SQRT(POWER(MAX(LAT_N) - MIN(LAT_N),2) + POWER(MAX(LONG_W) - MIN(LONG_W), 2)),4)
+FROM STATION; --POWER IS NUMBER RAISED TO ANOTHER NUMBER example POWER(8,2) = 64
+
+-- 34. Given the CITY and COUNTRY tables, query the sum of the populations of all cities where the CONTINENT is 'Asia'.
+--Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+SELECT SUM(t1.population)
+FROM city AS t1
+JOIN country AS t2 ON t1.countrycode = t2.code
+WHERE t2.continent = 'asia';
+
+-- 35. Given the CITY and COUNTRY tables, query the names of all cities where the CONTINENT is 'Africa'.
+--Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+SELECT t1.name FROM city AS t1
+JOIN country AS t2 ON t1.countrycode = t2.code
+WHERE t2.continent = 'africa';
+
+-- 36. Given the CITY and COUNTRY tables, query the names of all the continents (COUNTRY.Continent) and their respective average city populations (CITY.Population) rounded down to the nearest integer.
+SELECT t2.continent, FLOOR(AVG(t1.population))
+FROM city AS t1
+JOIN country AS t2 ON t1.countrycode = t2.code
+GROUP BY t2.continent;
